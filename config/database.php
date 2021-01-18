fbdc6f282e4e4567be2918107440ca05d9bb3f93
Chrisphine10 <ondiekchrisphine@gmail.com>
2021-01-19 01:00:18 +0300

    alalala

--- config/database.php
+++ config/database.php
@@ -1,14 +1,14 @@
 <?php
 
 use Illuminate\Support\Str;
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
 return [
-    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-    $host = $url["host"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $database = substr($url["path"], 1);
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
